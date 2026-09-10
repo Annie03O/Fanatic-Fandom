689ada6730564108e0cbf5a0055bec97f57861aa
Annie03O <annielolofsson@gmail.com>
2026-09-10 20:42:04 +0200

    la till variabel som letar efter related

--- app/comedy/[slug]/related/page.tsx
+++ app/comedy/[slug]/related/page.tsx
@@ -14,15 +14,16 @@ export default function AllRelatedPage({genre}: Props) {
     const cast = series?.cast ?? [];
 
     console.log("series", series);
-    
+   
+    const related = series?.related?.find((t) => t.title);
 
-    if (!series) return <section>Series not found</section>
+    if (!series || !related) return <section>Series not found</section>
         
     
     return (
       <div className="flex flex-col  items-center bg-black w-[90%] min-h-[1000px]"> 
         
-        <RelatedPortal show={series} page={true}/>
+        <RelatedPortal title={related.title ?? ""} show={series} page={true}/>
       </div>
     )
 }
