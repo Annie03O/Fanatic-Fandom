36d00184c234b735e445161a8fadcf9ca8214f71
Annie03O <annielolofsson@gmail.com>
2026-09-10 16:35:12 +0200

    made title optional

--- app/models/types/Occupation.ts
+++ app/models/types/Occupation.ts
@@ -1,9 +1,7 @@
-export type Occupation = {
-    title: string,
+export type Occupation  {
+    title?: string,
     place?: string,
     started?: string,
     hasEnded: boolean;
     ended?: string;
 }
-
-export type Education = Pick<Occupation, "place" | "hasEnded" | "ended" | "started" >
