5bf08b9999b579d98f22fea25333d78903b32c97
Annie03O <annielolofsson@gmail.com>
2026-09-16 18:35:56 +0200

    Changed function not to transform 's to 'S

--- app/functions/Format/capitalizeFirstLetter.ts
+++ app/functions/Format/capitalizeFirstLetter.ts
@@ -1,11 +1,11 @@
 // Source - https://stackoverflow.com/a/1026087
 // Posted by Steve Harrison, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-09-08, License - CC BY-SA 4.0
-
 export function capitalizeFirstLetter(val: string) {
-    
     return String(val)
         .toLowerCase()
-        .replace(/\b\w/g, char => char.toUpperCase());
-
+        .replace(/\b\w/g, char => char.toUpperCase())
+        .replace(/([a-z])'([a-z])/gi, (_, before, after) => {
+            return `${before}'${after.toLowerCase()}`;
+        });
 }
